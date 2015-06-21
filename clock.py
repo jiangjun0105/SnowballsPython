bb24cbce68ce1574e1596ce47a03cda6ec02b189
Jiang Jun <jiangjun0105@gmail.com>
2015-06-21 09:46:17 +0900

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,11 @@
+# Import the datetime library
+from datetime import datetime
+
+# Create a variable called current and assign it the value
+# of the function now() from the datetime library
+current = datetime.now()
+
+# Display only a part of the current time on each line
+print current.year
+print current.month
+print current.day
